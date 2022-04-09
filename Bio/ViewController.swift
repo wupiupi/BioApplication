6c389ed523e404a2e7a12b34ae1b8b87e96bf29c
Jormungandrp <90474212+Jormungandrp@users.noreply.github.com>
2022-04-09 15:18:26 +0300

    Initial Commit

---
+++ Bio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bio
+//
+//  Created by Paul on 9.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
